47e84f12094aec3e24ba49fe8b34bbed9e652fb4
Cagan Eldenk <caganeldenk@gmail.com>
2025-06-03 21:44:13 +0300

    Finished developing header

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-# starting formulasier
+import tkinter as tk
+from tkinter.ttk import Combobox
+
+# appsettling
+app = tk.Tk()
+app.title("Formulasier - Automatic Formula Calculator")
+app.geometry("750x500")
+app.resizable(False, False)
+
+
+# header
+header = tk.LabelFrame(app, width=740, height=75, background="#ccc")
+header.place(x=5, y=5)
+
+h1 = tk.Label(header, text="Formulasier - Automatic Formula Calculator", font="times 15 bold", background="#ccc")
+h1.place(x=5, y=20)
+
+
+# menu
+menuItemText = Combobox(app, text="Lessons...")
+menuItemText.place(x=5, y=80)
+
+
+app.mainloop()
